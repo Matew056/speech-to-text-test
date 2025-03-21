1dc452b09e8174b1a994e86583867b6c57eb8ef9
Mateo Etchepare <142445190+Matew056@users.noreply.github.com>
2025-03-21 12:15:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const startButton = document.getElementById('startButton');
+const outputDiv = document.getElementById('output');
+const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+
+recognition.interimResults = true;
+recognition.continuous = true;
+
+startButton.addEventListener('click', () => {
+    recognition.start();
+    startButton.disabled = true;
+    startButton.textContent = 'Recording...';
+});
+
+recognition.onresult = event => {
+    /*const result = event.results[event.results.length - 1][0].transcript;
+    //outputDiv.textContent = result;
+*/
+    const result = event.results[event.results.length - 1];
+    if (result.isFinal) { // Only append final results
+        outputDiv.textContent += result[0].transcript + ' ';
+    }
+};
+
+recognition.onend = () => {
+    startButton.disabled = false;
+    startButton.textContent = 'Start Recording';
+};
+
+recognition.onerror = event => {
+    console.error('Speech recognition error:', event.error);
+};
+
+recognition.onnomatch = () => {
+    console.log('No speech was recognized.');
+};
